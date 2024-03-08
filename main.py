01c74a69bfd084e1d44475c7c89d1b1e8022d688
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2024-03-08 18:31:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+from flask import Flask,session, render_template,request,url_for, redirect 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_session import Session
+import requests
