4e02f1ab9c80baa934bf1ab083256ff29c6b84b3
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2024-03-08 18:43:48 +0530

    folders added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask,session, render_template,request,url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager,login_user
 from flask_session import Session
+import openai
+
 
 app= Flask(__name__)
-app.config["SECRET_KEY"] ="AtigyiUS9812892019IKOSNJSGDU"
+app.config["SECRET_KEY"] ="AtigyiUS9812892024IKOSNJSGDU"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
