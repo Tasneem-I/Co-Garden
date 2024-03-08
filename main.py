9409661cd81ba2a671bb37cb4d961b8ebcc5bd31
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2024-03-08 18:37:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask,session, render_template,request,url_for, redirect 
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager,login_user
 from flask_session import Session
 
+app= Flask(__name__)
+app.config["SECRET_KEY"] ="AtigyiUS9812892019IKOSNJSGDU"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.init_app(app)
