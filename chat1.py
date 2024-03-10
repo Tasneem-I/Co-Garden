de484e5342318fcb5d28e94344fcbd5b2b85fae0
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2024-03-10 13:05:56 +0530

    Update chat1.py

--- chat1.py
+++ chat1.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.stylable_container import stylable_container 
+from streamlit_chat import message 
 
 bg="""
 <style>
